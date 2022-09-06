993dc0040846b44b18493de7f9943f564d19a90a
hkaankizildag <hkizildag@partner.innova.com.tr>
2022-09-06 20:50:27 +0300

    satis service testleri urun ile guncellendi

--- satis-service/src/test/java/com/marul/satis/SatisServiceTest.java
+++ satis-service/src/test/java/com/marul/satis/SatisServiceTest.java
@@ -4,6 +4,8 @@ import com.marul.dto.MusteriDto;
 import com.marul.dto.SatisDto;
 import com.marul.dto.result.SuccessDataResult;
 import com.marul.exception.BulunamadiException;
+import com.marul.urun.UrunDto;
+import com.marul.urun.UrunService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,6 +18,10 @@ import org.springframework.context.annotation.Description;
 import java.util.Collections;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class SatisServiceTest {
 
@@ -25,6 +31,8 @@ class SatisServiceTest {
     private MusteriFeignClient musteriFeignClient;
     @Mock
     private SatisMapper satisMapper;
+    @Mock
+    private UrunService urunService;
 
     @InjectMocks
     private SatisService satisService;
@@ -41,11 +49,11 @@ class SatisServiceTest {
         satis.setUrunId(urunId);
         satis.setId(satisId);
 
-        Mockito.when(satisRepository.findAll()).thenReturn(Collections.singletonList(satis));
+        when(satisRepository.findAll()).thenReturn(Collections.singletonList(satis));
 
         MusteriDto musteriDto = new MusteriDto();
         musteriDto.setId(musteriId);
-        Mockito.when(musteriFeignClient.findById(satis.getMusteriId())).thenReturn(new SuccessDataResult<>(musteriDto));
+        when(musteriFeignClient.findById(satis.getMusteriId())).thenReturn(new SuccessDataResult<>(musteriDto));
 
         //when
         List<SatisDto> satisDtoList = satisService.findAll();
@@ -58,7 +66,7 @@ class SatisServiceTest {
         Assertions.assertEquals(beklenen.size(), satisDtoList.size());
         Assertions.assertEquals(beklenen.get(0).getMusteriDto().getMusteriAdi(),
                 satisDtoList.get(0).getMusteriDto().getMusteriAdi());
-        Mockito.verify(satisRepository).findAll();
+        verify(satisRepository).findAll();
     }
 
     @Test
@@ -78,17 +86,20 @@ class SatisServiceTest {
         satis.setMusteriId(musteriId);
         satis.setUrunId(urunId);
 
-        Mockito.when(satisMapper.getEntity(satisDto))
+        when(satisMapper.getEntity(satisDto))
                 .thenReturn(satis);
 
-        Mockito.when(musteriFeignClient.findById(musteriId))
+        when(musteriFeignClient.findById(musteriId))
                 .thenReturn(new SuccessDataResult<>(musteriDto));
 
-        Mockito.when(satisRepository.save(satis))
+        when(satisRepository.save(satis))
                 .thenReturn(satis);
 
-        Mockito.when(satisMapper.getDto(satis))
+        when(satisMapper.getDto(satis))
                 .thenReturn(satisDto);
+
+        when(urunService.findById(urunId))
+                .thenReturn(new UrunDto());
 
         // when
         SatisDto actual = satisService.save(satisDto);
@@ -98,10 +109,11 @@ class SatisServiceTest {
         Assertions.assertEquals(actual.getMusteriDto().getEmail(), satisDto.getMusteriDto().getEmail());
         Assertions.assertEquals(actual.getUrunId(), satisDto.getUrunId());
 
-        Mockito.verify(satisMapper).getEntity(satisDto);
-        Mockito.verify(musteriFeignClient).findById(musteriId);
-        Mockito.verify(satisRepository).save(satis);
-        Mockito.verify(satisMapper).getDto(satis);
+        verify(satisMapper).getEntity(satisDto);
+        verify(musteriFeignClient).findById(musteriId);
+        verify(urunService).findById(urunId);
+        verify(satisRepository).save(satis);
+        verify(satisMapper).getDto(satis);
     }
 
     @Test
@@ -120,19 +132,56 @@ class SatisServiceTest {
         satis.setMusteriId(musteriId);
         satis.setUrunId(urunId);
 
-        Mockito.when(satisMapper.getEntity(satisDto))
+        when(satisMapper.getEntity(satisDto))
                 .thenReturn(satis);
 
-        Mockito.when(musteriFeignClient.findById(musteriId))
+        when(musteriFeignClient.findById(musteriId))
                 .thenThrow(BulunamadiException.class);
 
         // when
-        Assertions.assertThrows(BulunamadiException.class, () -> satisService.save(satisDto));
+        assertThrows(BulunamadiException.class, () -> satisService.save(satisDto));
 
         //then
-        Mockito.verify(satisMapper).getEntity(satisDto);
-        Mockito.verify(musteriFeignClient).findById(musteriId);
-        Mockito.verify(satisRepository, Mockito.times(0)).save(satis);
-        Mockito.verify(satisMapper, Mockito.times(0)).getDto(satis);
+        verify(satisMapper).getEntity(satisDto);
+        verify(musteriFeignClient).findById(musteriId);
+        verify(satisRepository, Mockito.times(0)).save(satis);
+        verify(satisMapper, Mockito.times(0)).getDto(satis);
     }
+
+    @Test
+    void itShouldNot_Save_WhenProductIsNotExists() {
+        // given
+        SatisDto satisDto = new SatisDto();
+        MusteriDto musteriDto = new MusteriDto();
+        long musteriId = 1L;
+        long urunId = 1L;
+        musteriDto.setId(musteriId);
+
+        satisDto.setMusteriDto(musteriDto);
+        satisDto.setUrunId(urunId);
+
+        Satis satis = new Satis();
+        satis.setMusteriId(musteriId);
+        satis.setUrunId(urunId);
+
+        when(satisMapper.getEntity(satisDto))
+                .thenReturn(satis);
+
+        when(musteriFeignClient.findById(musteriId))
+                .thenReturn(new SuccessDataResult<>(musteriDto));
+
+        when(urunService.findById(urunId))
+                .thenThrow(BulunamadiException.class);
+
+        // when
+        assertThrows(BulunamadiException.class, () -> satisService.save(satisDto));
+
+        //then
+        verify(satisMapper).getEntity(satisDto);
+        verify(musteriFeignClient).findById(musteriId);
+        verify(urunService).findById(urunId);
+        verify(satisRepository, Mockito.times(0)).save(satis);
+        verify(satisMapper, Mockito.times(0)).getDto(satis);
+    }
+
 }
