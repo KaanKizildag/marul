82ee1bc4f85213cf5d6a0605a9f98afcfa34bb72
hkaankizildag <hkizildag@partner.innova.com.tr>
2022-10-08 17:31:31 +0300

    security cors hatasi duzeltildi

--- marul-frontend/src/services/ProductService.js
+++ marul-frontend/src/services/ProductService.js
@@ -10,6 +10,20 @@ export class ProductService {
         return axios.get(BASE_URL + "/satis-service/v1/urun/findAll")
     }
 
+    savePrdocut(product) {
+        return axios.post(BASE_URL + "/satis-service/v1/urun/save", product, {
+            headers: {
+                'Content-Type': 'application/json',
+                "Access-Control-Allow-Origin": 'http://127.0.0.1:5173/',
+                "Access-Control-Allow-Credentials": "true",
+                "Access-Control-Expose-Headers": "Set-Cookie",
+                "Access-Control-Allow-Headers": "Content-Type, x-xsrf-token, X-Requested-With, Accept, Expires, Last-Modified, Cache-Control",
+                "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+                'Accept': '*/*'
+            },
+        })
+    }
+
     haftalikSatislariGetir() {
         return axios.get(BASE_URL + "/satis-service/v1/satis/haftalik-satislari-getir")
     }
--- marul-frontend/vite.config.js
+++ marul-frontend/vite.config.js
@@ -3,16 +3,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 8000
-    //   proxy: {
-    //     '/v1': {
-    //       target: 'https://localhost:8084',
-    //       changeOrigin: true,
-    //       secure: false,
-    //       ws: true,
-    //     }
-    //   }
-  }
+    plugins: [vue()],
+    server: {
+        proxy: {
+            '/**': {
+                target: 'http://localhost:8084',
+                changeOrigin: true,
+                secure: false,
+                ws: true,
+            }
+        }
+    }
 })
+
+
