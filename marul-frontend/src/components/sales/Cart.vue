f4f73ddeeca25c897fab7dd3d25f54e294a2350e
turgay-dede <turgay.dede28@hotmail.com>
2022-11-06 14:31:13 +0300

    sales page refactored

--- marul-frontend/src/components/sales/Cart.vue
+++ marul-frontend/src/components/sales/Cart.vue
@@ -3,14 +3,23 @@
     <div class="card h-100 mb-4">
       <div class="card-header pb-0 px-3">
         <h6 class="mb-0">Sepet</h6>
-        <Table :data="saleArray"
+        <Table :data="cartArray"
                style="width:100%;">
           <el-table-column label="Ürün Adı" prop="urunAdi"/>
-          <el-table-column label="Adet" prop="satilanAdet"/>
-          <el-table-column label="Tutar" prop="tutar"/>
-          <el-table-column align="right">
+          <el-table-column label="Adet" align="center" >
             <template #default="scope">
-              <DeleteButton @click="removeFromCart(scope.row)"/>
+              <el-input-number
+                  v-model="scope.row.satilanAdet"
+                  :min="0"
+                  :max="1000000"
+                  size="small"
+                  @change="handleChange(scope.row)"
+              />
+            </template>
+          </el-table-column>
+          <el-table-column label="Tutar" prop="tutar">
+            <template #default="scope">
+              {{scope.row.satilanAdet * scope.row.tutar}}
             </template>
           </el-table-column>
         </Table>
@@ -24,26 +33,22 @@
 
 <script setup>
 import Table from "../common/Table.vue";
-import DeleteButton from "../common/DeleteButton.vue";
 import {computed, ref} from "vue";
 
-const props = defineProps(["saleArray"])
-const emits = defineEmits(["removeFromCart"])
+const props = defineProps(["cartArray"])
+const emits = defineEmits(["changeProductCount"])
 
-const removeFromCart = (row) => {
-  emits("removeFromCart",row)
-}
 const total = ref(0)
+
+const handleChange = (row) => {
+  emits("changeProductCount",row)
+}
 
 const totalPrice = computed(()=> {
   total.value = 0;
-  props.saleArray.forEach(sale => {
-    total.value += sale.tutar;
+  props.cartArray.forEach(sale => {
+    total.value += sale.satilanAdet * sale.tutar;
   })
   return total.value;
 })
 </script>
-
-<style scoped>
-
-</style>
