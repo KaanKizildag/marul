ffd52b456a1c5be70b4ba67b44b8fbbb6f2f10e6
hkaankizildag <hkizildag@partner.innova.com.tr>
2023-04-06 00:23:39 +0300

    tur icin testler eklendi

---
+++ musteri-service/src/test/java/com/marul/tur/TurControllerTest.java
@@ -0,0 +1,82 @@
+package com.marul.tur;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class TurControllerTest {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Mock
+    private TurService turService;
+
+    @InjectMocks
+    private TurController turController;
+
+    private List<TurDto> turList;
+
+    private TurDto turDto;
+
+    @BeforeEach
+    public void setUp() {
+        turList = new ArrayList<>();
+        turDto = new TurDto();
+        turDto.setId(1L);
+        turDto.setTurAdi("Tur1");
+        turList.add(turDto);
+        mockMvc = MockMvcBuilders.standaloneSetup(turController).build();
+    }
+
+    @Test
+    void findAllTest() throws Exception {
+        when(turService.findAll()).thenReturn(turList);
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/tur/findAll"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        verify(turService, times(1)).findAll();
+    }
+
+    @Test
+    void findByIdTest() throws Exception {
+        when(turService.findById(any())).thenReturn(turDto);
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/tur/findById/{id}", 1L))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        verify(turService, times(1)).findById(anyLong());
+    }
+
+    @Test
+    void saveTest() throws Exception {
+        String turDtoJson = objectMapper.writeValueAsString(turDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/v1/tur/save")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(turDtoJson))
+                .andExpect(status().isOk());
+
+        // verify the service method was called
+        verify(turService, times(1)).save(any());
+    }
+}
---
+++ musteri-service/src/test/java/com/marul/tur/TurServiceTest.java
@@ -0,0 +1,126 @@
+package com.marul.tur;
+
+import com.marul.exception.NotFoundException;
+import com.marul.exception.ZatenKayitliException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TurServiceTest {
+
+    @Mock
+    private TurMapper turMapper;
+
+    @Mock
+    private TurRepository turRepository;
+
+    @InjectMocks
+    private TurService turService;
+
+    @Test
+    void testFindAll() {
+        // Given
+        List<Tur> turList = Arrays.asList(new Tur(), new Tur());
+        List<TurDto> turDtoList = Arrays.asList(new TurDto(), new TurDto());
+        when(turRepository.findAll()).thenReturn(turList);
+        when(turMapper.getTargetList(turList)).thenReturn(turDtoList);
+
+        // When
+        List<TurDto> result = turService.findAll();
+
+        // Then
+        assertEquals(turDtoList, result);
+        verify(turRepository).findAll();
+        verify(turMapper).getTargetList(turList);
+    }
+
+    @Test
+    void testFindById() {
+        // Given
+        Long id = 1L;
+        Tur tur = new Tur();
+        TurDto turDto = new TurDto();
+        when(turRepository.findById(id)).thenReturn(Optional.of(tur));
+        when(turMapper.getTarget(tur)).thenReturn(turDto);
+
+        // When
+        TurDto result = turService.findById(id);
+
+        // Then
+        assertEquals(turDto, result);
+        verify(turRepository).findById(id);
+        verify(turMapper).getTarget(tur);
+    }
+
+    @Test
+    void testFindByIdNotFound() {
+        // Given
+        Long id = 1L;
+        when(turRepository.findById(id)).thenReturn(Optional.empty());
+
+        // When
+        Assertions.assertThrows(NotFoundException.class, () -> turService.findById(id));
+
+        // Then an exception is thrown
+    }
+
+    @Test
+    void testExistsByTurId() {
+        // Given
+        Long id = 1L;
+        when(turRepository.existsById(id)).thenReturn(true);
+
+        // When
+        boolean result = turService.existsByTurId(id);
+
+        // Then
+        assertTrue(result);
+        verify(turRepository).existsById(id);
+    }
+
+    @Test
+    void testSave() {
+        // Given
+        String turAdi = "Tur Adı";
+        TurDto turDto = new TurDto();
+        turDto.setTurAdi(turAdi);
+        when(turRepository.existsByTurAdi(turAdi)).thenReturn(false);
+        Tur tur = new Tur();
+        when(turMapper.getSource(turDto)).thenReturn(tur);
+
+        // When
+        turService.save(turDto);
+
+        // Then
+        verify(turRepository).existsByTurAdi(turAdi);
+        verify(turMapper).getSource(turDto);
+        verify(turRepository).save(tur);
+    }
+
+    @Test
+    void testSaveAlreadyExists() {
+        // Given
+        String turAdi = "Tur Adı";
+        TurDto turDto = new TurDto();
+        turDto.setTurAdi(turAdi);
+        when(turRepository.existsByTurAdi(turAdi)).thenReturn(true);
+
+        // When
+        Assertions.assertThrows(ZatenKayitliException.class, () -> turService.save(turDto));
+
+        // Then an exception is thrown
+    }
+}
